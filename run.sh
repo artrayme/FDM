7a09cb503f328132ea1a98762c3d19a721c95aa9
artrayme <artrayme@protonmail.com>
2021-11-22 20:49:10 +0300

    docs: add start-up manual

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sh ./gradlew bootJar
-cp build/PBZ_lr2-0.0.1-SNAPSHOT.jar docker
+cp build/libs/PBZ_lr2-0.0.1-SNAPSHOT.jar docker
 cd docker
 docker build -t fdm .
 docker-compose up
-
+rm docker/PBZ_lr2-0.0.1-SNAPSHOT.jar
