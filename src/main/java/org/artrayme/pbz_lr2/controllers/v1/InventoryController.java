2c7b77da3bc97c59645f6f73b3abc4799a84b178
artrayme <artrayme@protonmail.com>
2021-11-22 20:20:46 +0300

    fix: remove unused files

--- src/main/java/org/artrayme/pbz_lr2/controllers/v1/InventoryController.java
+++
@@ -1,7 +0,0 @@
-package org.artrayme.pbz_lr2.controllers.v1;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping(path = "/inventory")
-public class InventoryController {
-}
